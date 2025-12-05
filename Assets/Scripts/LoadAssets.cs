ec724079a2ecfe61bb1f78eace8aa73797e9543a
macsk <mac.skaanes@gmail.com>
2025-12-04 23:54:13 -0500

    first commit

---
+++ Assets/Scripts/LoadAssets.cs
@@ -0,0 +1,26 @@
+using System;
+using System.IO;
+using UnityEngine;
+
+public class LoadAssets : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        try
+        {
+            string contents = File.ReadAllText(Application.streamingAssetsPath+"/test.txt");
+            // print(Application.streamingAssetsPath+"/test.txt");
+            print("Contents of Test.txt");
+            print(contents);
+        }
+        catch (FileNotFoundException e)
+        {
+            print("Couldn't load file");
+        }
+        catch (Exception e)
+        {
+            print("Error Occured failed to load file");
+        }
+    }
+}
